89b986d1107ad9f54bb23d51e627f5a0a2d09dd8
suosuojiang <995691906@qq.com>
2019-03-19 17:37:07 +0800

    支付模块完成

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -7,8 +7,11 @@ $(function(){
     //将获取到的URL参数键值对里的value赋值给type变量
     var type    = _mm.getUrlParam('type') || 'default',
     element     = $('.' + type + '-success');
+    if (type === 'payment') {
+        var orderNumber = _mm.getUrlParam('orderNumber');
+            $orderNumber = $element.find('.order-number');
+        $orderNumber.attr('href', $orderNumber.attr('href') + orderNumber);
+    }
     // 显示对应的提示元素
-    element.show();
-
-    
+    element.show();    
 })
